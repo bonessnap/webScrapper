9139cd3ab963b136cb0de4c0780c08adf66271df
Кенни Бонснап <katfokshakat@gmail.com>
2023-04-23 19:39:37 +0300

    4.2

--- parsers/components/init_browser.py
+++ parsers/components/init_browser.py
@@ -1,28 +1,30 @@
-
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
+import chromedriver_autoinstaller
 
-# тут создается обьект браузер с настройками и возвращается в парсер
 def getBrowser():
-    # настройки браузера
+    # Проверка версии драйвера Chrome и браузера
+    chromedriver_autoinstaller.install()
+
+    # Настройки браузера
     chrome_options = Options()
     chrome_options.add_argument("--window-size=1920,1080")
     chrome_options.add_argument("--start-maximized")
-    # без открытия окна (тихий режим)
-    chrome_options.add_argument("--headless")
-    # без логирования
+    chrome_options.add_argument("--headless") # Режим без открытия окна (тихий режим)
     chrome_options.add_argument("--disable-logging")
-    # без трассировки
     chrome_options.add_argument("--disable-in-process-stack-traces")
-    # без крашрепорта
     chrome_options.add_argument("--disable-crash-reporter")
-    # разрешить жс
     chrome_options.add_argument("--enable-javascript")
     chrome_options.add_argument("--disable-gpu")
     chrome_options.add_argument("--disable-software-rasterizer")
-    chrome_options.add_experimental_option(
-        'excludeSwitches', ['enable-logging'])
-    chrome_options.add_argument('--disable-extensions')
+    chrome_options.add_argument("--no-sandbox") # Для безопасности в режиме headless
+    chrome_options.add_experimental_option('excludeSwitches', ['enable-logging'])
+    chrome_options.add_argument("--disable-extensions")
   
-    BROWSER = webdriver.Chrome(options=chrome_options)
-    return BROWSER
+    try:
+        # Создание объекта браузера
+        browser = webdriver.Chrome(options=chrome_options)
+        return browser
+    except:
+        print("Failed to create browser instance!")
+        return None
